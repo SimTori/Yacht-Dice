8613a54e60eb65c4f6da06862c87e91059e61da5
SimTori <cjh6574@gmail.com>
2022-11-17 22:04:23 +0900

    container, header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"container"}>
+        <div className="board">
+            <div className="header">
+                <div className="title">
+                    <a href="">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <h1 className="board-title">
+                        Yacht Dice
+                    </h1>
+                    <a href="">
+                        <i className="far fa-question-circle"></i>
+                    </a>
+                </div>
+                <div className="dice">
+                    <div className="dice-container">
+                        <i className="die fas fa-dice-two "></i>
+                        <i className="die fas fa-dice-two "></i>
+                        <i className="die fas fa-dice-two "></i>
+                        <i className="die fas fa-dice-two "></i>
+                        <i className="die fas fa-dice-two "></i>
+                    </div>
+                    <button className="dice-roll-button">
+                        2 Rolls Left
+                    </button>
+                </div>
+            </div>
+
+            <div className="sheet">
+
+            </div>
+        </div>
+      </div>
   );
 }
 
