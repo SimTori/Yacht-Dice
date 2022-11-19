558e03d06db35f84f4317db9b15c8d09bb3336e7
SimTori <cjh6574@gmail.com>
2022-11-19 14:32:28 +0900

    스코어보드 레이아웃

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,12 +1,12 @@
 import React from "react";
 import 'semantic-ui-css/semantic.min.css'
-import {Container} from "semantic-ui-react";
+import {Container, Button} from "semantic-ui-react";
 
 class GameBoard extends React.Component {
     render() {
         return(
             <Container>
-
+                <Button>Rolls</Button>
             </Container>
         )
     }
